4873c24574b3a787f9b70cbed1df31d579f69693
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-03 20:00:51 -0300

    Fix NewNote styles surfacing
    Gotta call the function

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -2,7 +2,7 @@ import type { LinksFunction } from "@remix-run/node";
 
 import NewNote, { links as NewNoteLinks } from "~/components/NewNote";
 
-export const links: LinksFunction = () => [ ...NewNoteLinks ];
+export const links: LinksFunction = () => [...NewNoteLinks()];
 
 export default function Demo() {
   return (
