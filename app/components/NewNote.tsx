aa1ddc6bf8442f8513760a02856bb0dfe2770960
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-03 19:50:15 -0300

    Add `NewNote` component with its styles

---
+++ app/components/NewNote.tsx
@@ -0,0 +1,29 @@
+import styles from "./NewNote.css";
+
+import type { LinksFunction } from "@remix-run/node";
+
+// surface styles: they are still global, but this way you have them close to
+// where they are used
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
+
+export default function NewNote() {
+  return (
+    <form method="post" id="note-form">
+      <p>
+        <label htmlFor="title">Title</label>
+        <input type="text"id="title" name="title"required />
+      </p>
+
+      <p>
+        <label htmlFor="content">Content</label>
+        <textarea id="content" name="content" rows={5} required />
+      </p>
+
+      <div className="form-actions">
+        <button>Add Note</button>
+      </div>
+    </form>
+  );
+}
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,10 +1,13 @@
-import { Link } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import NewNote, { links as NewNoteLinks } from "~/components/NewNote";
+
+export const links: LinksFunction = () => [ ...NewNoteLinks ];
 
 export default function Demo() {
   return (
     <main>
-      <h1>Notes</h1>
-      <Link to="/">Home</Link>
+      <NewNote />
     </main>
   );
 }
