0db49735b830fd0988bd7320f50ed488ec0c773c
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-03 21:25:41 -0300

    Basic note saving implementation
    No error handling

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,8 +1,28 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, ActionFunctionArgs  } from "@remix-run/node";
+import type { Note } from "~/types/Note";
+
+import invariant from 'tiny-invariant';
 
 import NewNote, { links as NewNoteLinks } from "~/components/NewNote";
+import db from "~/data/db";
 
 export const links: LinksFunction = () => [...NewNoteLinks()];
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const note = Object.fromEntries(formData.entries()) as Note;
+  // `title` can be null, `content` not
+  invariant(note.content, "Please provide some content for the note");
+
+  const { noteList } = db.data;
+  noteList.push(note);
+  await db.write()
+
+  return new Response(null, {
+    status: 200,
+    statusText: "ok",
+  });
+};
 
 export default function Demo() {
   return (
