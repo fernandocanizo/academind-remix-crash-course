f7e48dafe687787bc2c2f9382b069fcbe7d52d7d
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-06 22:01:43 -0300

    Modify `Note` type: add id
    `id` field build with `short-unique-id` with an embedded timestamp.

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -2,6 +2,7 @@ import type { LinksFunction, ActionFunctionArgs  } from "@remix-run/node";
 import type { Note } from "~/types/Note";
 
 import invariant from 'tiny-invariant';
+import ShortUniqueId from 'short-unique-id';
 
 import NewNote, { links as NewNoteLinks } from "~/components/NewNote";
 import db from "~/data/db";
@@ -9,10 +10,13 @@ import db from "~/data/db";
 export const links: LinksFunction = () => [...NewNoteLinks()];
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const { stamp } = new ShortUniqueId();
   const formData = await request.formData();
   const note = Object.fromEntries(formData.entries()) as Note;
   // `title` can be null, `content` not
   invariant(note.content, "Please provide some content for the note");
+  const uidWithTimestamp = stamp(10);
+  note.id = uidWithTimestamp;
 
   const { noteList } = db.data;
   noteList.push(note);
